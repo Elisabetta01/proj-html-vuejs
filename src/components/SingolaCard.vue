e1bd6c6cdbcae54cc3678398379d3341800754ad
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-14 16:41:10 +0200

    card

---
+++ src/components/SingolaCard.vue
@@ -0,0 +1,42 @@
+<script>
+     export default {
+          name: "SingolaCard",
+          props: ['propsCard'],
+     }
+</script>
+
+<template>
+
+     <div id="card" class="d-flex">
+          <div class="w-50">
+               <img :src="propsCard.img" alt="img">
+          </div>
+          
+          <div id="testo" class="w-25">
+               <h2 class="my-3">{{ propsCard.titolo }}</h2>
+               <p>{{ propsCard.testo }}</p>
+               <button type="button" class="btn btn-warning"> {{ propsCard.button }}</button>
+          </div>
+          
+     </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+     #card{
+          margin-bottom: 100px;
+          img{
+               width: 100%;
+               height: 100%;
+          }
+          #testo{
+               font-family: "Times New Roman", Times, serif;
+               margin-left: 70px;
+               h2{
+                    color: #eb621f;
+               }
+          }
+     }
+
+</style>
---
+++ src/components/WhyusComp.vue
@@ -0,0 +1,49 @@
+<script>
+
+     import SingolaCard from './SingolaCard.vue';
+
+     export default {
+          name:"WhyusComp",
+          components: {
+               SingolaCard,
+          },
+          data(){
+               return{
+                    whyus: [
+                         {
+                              img: "/public/img/unicità_i_design.jpg",
+                              titolo: "UNICITA'",
+                              testo:"Spinti da una forte passione per il design, bello e funzionale, ci piace ispirare e influenzare ogni nostro progetto per rendere unico il tuo spazio.",
+                              button: "Chi siamo"
+                         },
+                         {
+                              img: "/public/img/ispirazione_i_design.webp",
+                              titolo: "ISPIRAZIONE",
+                              testo:"Lasciati trasportare dalla creatività e immergiti in un suggestivo viaggio tra i prodotti più esclusivi e l’eccellenza del made in Italy e del design internazionale.",
+                              button: "Chi siamo"
+                         },
+                         {
+                              img: "/public/img/internazionalità_i_design.avif",
+                              titolo: "INTERNAZIONALITA'",
+                              testo:"Da oltre 50 anni collaboriamo con partner in tutto il mondo. Una contaminazione che permette di avere un approccio attento e diversificato per rispondere ad ogni tipo di esigenza.",
+                              button: "Chi siamo"
+                         },
+                    ]
+               }
+          },
+     }
+</script>
+
+<template>
+     <div class=" container w-75 m-auto mt-4">
+          <div id="container-whyus" >
+               <SingolaCard v-for="(element, index) in whyus" :key="index" :propsCard="element"/>
+          </div>
+
+     </div>
+</template>
+
+
+<style lang="scss" scoped>
+     
+</style>
