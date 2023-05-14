eedad0e15b05863a53926761c22598b01f6ee1c1
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-14 11:12:47 +0200

    HeaderComp

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -3,16 +3,97 @@
           name: "HeaderComp",
           data(){
                return{
-                    
+                    menu: [
+                         {
+                              nome: 'Home',
+                              current: true
+                         },
+                         {
+                              nome: 'Services',
+                              current: false
+                         },
+                         {
+                              nome: 'Why Us',
+                              current: false
+                         },
+                         {
+                              nome: 'Portfolio',
+                              current: false
+                         },
+                         {
+                              nome: 'About',
+                              current: false
+                         },
+                         {
+                              nome: 'Magazine',
+                              current: false
+                         },
+
+                    ]
                }
           }
      }
 </script>
 
 <template>
-     <h1></h1>
+     
+     <header>
+     <nav>
+          <img src="/img/pantone.png" alt="logo">
+
+          <ul>
+               <li v-for="(element, index) in menu" :key="index"
+               :class="element.current ? 'active' : '' ">
+                    <a href="">{{ element.nome }}</a>
+               </li>
+          </ul>
+          
+
+     </nav>
+  </header>
+
 </template>
 
 <style lang="scss" scoped>
+
+     header{
+          height: 100px;
+          nav{
+               display: flex;
+               justify-content: space-around;
+               align-items: center;
+               width: 80%;
+               margin: 0 auto;
+               img{
+                    width: 80px;
+               }
+               ul{
+                    display: flex;
+                    li{
+                         list-style: none;
+                         margin: 0 10px;
+                         line-height: 100px;
+                         a{
+                              text-decoration: none;
+                              color: black;
+                              text-transform: uppercase; 
+                         }
+                         &.active {
+                              border-bottom: 5px solid #eb621f; 
+                         }
+                         &.active a {
+                              color: #eb621f;
+                         }
+                         &:hover {
+                              border-bottom: 5px solid #eb621f; 
+                         }
+                         &:hover a{
+                              color: #eb621f; 
+                         }
+                    }
+               
+               }
+          }
+     }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@ import App from './App.vue'
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import { phone } from '@fortawesome/free-solid-svg-icons'
+// library.add(phone)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
