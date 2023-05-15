484113088861e2e626d48973f62c63708f35b88f
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-15 21:15:35 +0200

    awards

--- src/components/TestimonialComp.vue
+++ src/components/TestimonialComp.vue
@@ -1,6 +1,32 @@
 <script>
      export default {
           name: "TestimonialComp",
+          data(){
+               return{
+                    awards: [
+                         {
+                              img: "/img/award-a.png",
+                              testo: 'Winner Seo Master MAGT',
+                              testo2: 'Smart Start Award 2017',
+                         },
+                         {
+                              img: "/img/award-b.png",
+                              testo: 'Top Interior Design Agencies',
+                              testo2: 'Next Partner 2018',
+                         },
+                         {
+                              img: "/img/award-c.png",
+                              testo: '10 Fastest Growing Artemide',
+                              testo2: 'Solution Provider 2019',
+                         },
+                         {
+                              img: "/img/award-d.png",
+                              testo: 'National Excellence Agencie',
+                              testo2: 'Award Winner 2020',
+                         },
+                    ]
+               }
+          }
      }
 </script>
 
@@ -9,9 +35,25 @@
           <div class="container w-50 d-flex justify-content-center">
                <img src="/img/large-testimonial.jpg" alt="" class="rounded shadow ">
                <div id="testo">
-                    <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure, corrupti iste soluta nobis cum autem, officiis qui laborum sed nihil ipsa ipsum architecto? Reprehenderit facilis expedita voluptate dolores quam."</p>
+                    <p class="fw-bold">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure, corrupti iste soluta nobis cum autem, officiis qui laborum sed nihil ipsa ipsum architecto? Reprehenderit facilis expedita voluptate dolores quam."</p>
                     <p>Kate Schadler </p>
                     <p class="smaller">Interior Designer - Artemide</p>
+
+               </div>
+          </div>
+
+          <div id="awards">
+               <div>
+                    <h1 class="text-center">Awards</h1>
+               </div>
+               <div class="d-flex justify-content-center">
+                    <div v-for="(element, index) in awards" :key="index" 
+                    class="text-center m-4">
+                         <img :src="element.img" alt="">
+                         <p>{{ element.testo }}</p>
+                         <p>{{ element.testo2 }}</p>
+     
+                    </div>
 
                </div>
           </div>
@@ -30,6 +72,17 @@
                .smaller{
                     font-size: 12px;
                }
+          }
+     }
+
+     #awards{
+          margin-top: 100px;
+          font-family: "Times New Roman", Times, serif;
+          h1{
+               color: #eb621f;
+          }
+          img{
+               width: 100px;
 
           }
      }
